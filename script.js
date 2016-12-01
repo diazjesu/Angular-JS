c6e441d8cfcd9cd579976036ba43b4c2930b835b
Jesus Diaz <diazjesu@my.dom.edu>
2016-12-01 00:52:11 -0600

    cleaned up code and added comments

--- script.js
+++ script.js
@@ -4,30 +4,29 @@
   // Create a module for latest version of Angular
   var app = angular.module("githubViewer", []);
 
-
-
   //
   // Main controller
   //
   var MainController = function($scope, $http) {
+    // $scope.variableName allows you to pass information for js to and from the html file
+    $scope.message = "Github Viewer";
+    $scope.username = "angular";
+
     var onUserComplete = function(response) {
+        // If response is successful
         $scope.user = response.data;
         $scope.error = ""; // Problem Solved :)
     };
 
     var onError = function(reason) {
+      // Error fetching data for username
       $scope.error = "Could not fetch the user";
     }
-
-    $scope.message = "Github Viewer";
 
     $scope.search = function(username) {
       $http.get("https://api.github.com/users/" + username)
         .then(onUserComplete, onError);
     };
-
-    $scope.username = "angular";
-
   };
 
   app.controller("MainController", MainController);
